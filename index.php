1479e086b888fdb99635d492e40aa51e967833a4
hamza03290 <161818517+hamza03290@users.noreply.github.com>
2024-04-04 02:58:46 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,212 @@
+<?php 
+session_start();
+     $_SESSION
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Hamza's Personal Portfolio Website</title>
+    <link rel="stylesheet" type="text/css" href="css/scripst.css">
+    <!-- Box icons link -->
+    <link rel="stylesheet" href="https://unpkg.com/boxicons@latest/css/boxicons.min.css">
+    <!-- Remix icons link -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/remixicon@3.5.0/fonts/remixicon.css">
+    <!-- Google Fonts link -->
+    <!-- Add your Google Fonts link here -->
+</head>
+<body>
+
+<!-- Header -->
+<header>
+    <a href="#" class="logo">Hamza <span>N.</span></a>
+    <div class="bx bx-menu" id="menu-icon"></div>
+
+    <ul class="navlist">
+        <li><a href="#over-mij">Over mij</a></li>
+        <li><a href="#kwaliteiten">Kwaliteiten</a></li>
+        <li><a href="#opleiding">opleiding</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="login.php"> login</a></li>
+    </ul>
+
+
+</header>
+<!---hero-->
+<section class="hero" id="homo">
+<div class="main-content">
+    <h4>Hallo.</h4>
+    <h1>Ik ben <span>Hamza Nuur </span></h1>
+    <p>Welkom bij mijn softwareontwikkelingsportfolio, waarin ik mijn expertise en prestaties op het gebied van softwareontwikkeling bij Scalda graag met u deel. Hier zijn enkele stappen om mijn portfolio naar een hoger niveau te tillen:</p>
+<div class= "social"> 
+        <a href="mailto:hamzanuur0620@gmail.com"><i class="ri-mail-fill"></i></a>
+        <a href="https://nl.linkedin.com/in/hamza-nuur-3bb11829b"><i class="ri-linkedin-fill"></i></a>
+    
+
+    </div>
+<div class="main.btn">
+    <a href="mailto:hamzanuur0620@gmail.com" class="btn">Hire me</a>
+    <a href="CV Hamza.pdf" class="btn btn2">Dowload CV</a>
+</div>
+</div>
+</section>  
+<!---Over-mij-->
+<section class="over-mij" id="over-mij">
+    <div class="over-mij-img">
+        <img src="images/Over-mij.jpg" alt="Over mij afbeelding">
+    </div>
+    <div class="Over-mij-text">
+        <h2> Ik ben een eerstejaars,  <span>sofware developer</span> <br> </h2>
+   
+        <p class="exp">
+            <span>Naam: Hamza Nuur</span>
+          Dit is mijn portofolie website 
+        </p>
+        <p class="exp">
+
+            Hallo, ik ben Hazma Nuur en ik woon in Terneuzen. Momenteel volg ik de opleiding Software Developer op het MBO Scalda, in de klas ICTSD1. Ik ben 16 jaar oud en woon nog bij mijn ouders. In mijn vrije tijd werk ik bij Albert Heijn, maar buiten school en werk breng ik graag mijn tijd door met gymmen.
+        </p>
+        <p class="exp">
+            <span>Address:</span>
+           Vlietstraat 11A
+        </p>
+        <p class="exp">
+            <span> Mail:</span>
+           Hamzanuur0620@gmail.com
+        </p>
+        <p class="exp">
+            <span>telefoon:</span>
+            0686427297
+        </p>
+        
+        
+    </div>
+    <a href="#portfolio" class="btn"> view all project</a>
+    </div>
+</section>
+<!---kwaliteiten-->
+<section class="kwaliteiten" id="kwaliteiten">
+    <div class="center-text">
+        <h2>mijn <span> kwaliteiten</span></h2>
+    </div>
+
+    <div class="kwaliteiten-content">
+    <div class="box">
+        <img src="images/s-1.svg">
+        <h3>Eerlijk</h3>
+        <p>Het is belangrijk om eerlijk te zijn, omdat eerlijkheid vertrouwen opbouwt en relaties sterker maakt. Wanneer we eerlijk zijn, kunnen anderen op ons rekenen en ontstaat er een basis voor een gezonde communicatie.</p>
+        <a href="#">
+           
+            <i class="ri-arrow-right-line"></i>
+        </a>
+    </div>
+        <div class="box">
+        <img src="images/s-2.svg">
+        <h3>Zelfstandig </h3>
+        <p>Het is belangrijk om zelfstandig te zijn, omdat zelfstandigheid ons in staat stelt om onafhankelijk te handelen en beslissingen te nemen op basis van onze eigen overtuigingen en behoeften. Dit vergroot onze zelfverzekerdheid en onze capaciteit om met uitdagingen om te gaan.</p>
+        <a href="#">
+          
+            <i class="ri-arrow-right-line"></i>
+        </a>
+    </div>
+        <div class="box">
+        <img src="images/s-3.svg">
+        <h3>Realistisch </h3>
+        <p>Het is belangrijk om realistisch te zijn, omdat realisme ons helpt om de wereld om ons heen te begrijpen en effectief te navigeren. Door realistisch te zijn, kunnen we situaties en uitdagingen objectief beoordelen, waardoor we betere beslissingen kunnen nemen.</p>
+        <a href="#">
+          
+            <i class="ri-arrow-right-line"></i> 
+        </a>
+    </div>
+</section>
+
+<!---opleiding-->
+<section class="opleiding" id="opleiding">
+    <div class="center-text">
+        <h2>mijn <span> opleiding</span></h2>
+    </div>
+
+    <div class="opleiding-content">
+    <div class="box">
+        <i class="ri-school-line"></i>
+        <h3>opleiding</h3>
+        <p>Ik volg momenteel de opleiding Software Development. Mijn vooropleiding omvat vier jaar op het Lodewijk College, waarvan de laatste twee jaar waren gewijd aan VKR (Verkoop, Klantenservice en Relatiebeheer) en het laatste jaar aan ICT. Tijdens deze periode heb ik diverse opdrachten uitgevoerd.
+
+            De reden dat ik voor deze opleiding heb gekozen, is omdat ik al ervaring heb opgedaan in het vakgebied van ICT en het me interessant lijkt om me te specialiseren in softwareontwikkeling. Bovendien streef ik naar het behalen van een diploma op niveau 4. Ik ben enthousiast over mijn huidige opleiding en kijk uit naar wat de toekomst in dit vakgebied voor mij in petto heeft.</p>
+    </div>
+</section>
+
+</section>
+<!---portfolio-->
+<section class="portfolio" id="portfolio">
+    <div class="center-text">
+        <h2>mijn <span>portfolio</span></h2>
+    </div>
+    <div class="portfolio-content">
+        <div class="row">
+            <img src="images/port-1.jpg">
+        <div class="main-row">
+            <div class="row-text">
+                <h5>Sprint 1</h5>
+            </div>
+            <div class="row-icon">
+                <i class="ri-github-fill"></i>
+            </div>
+            <h4>gemaakt door hamza</h4>
+        </div>
+        </div>
+
+        <div class="row">
+            <img src="images/port-2.jpg">
+        <div class="main-row">
+            <div class="row-text">
+                <h5>Sprint 2</h5>
+            </div>
+            <div class="row-icon">
+                <i class="ri-github-fill"></i>
+            </div>
+            <h4>gemaakt door hamza</h4>
+        </div>
+        </div>
+        <div class="row">
+            <img src="images/port-3.jpg">
+        <div class="main-row">
+            <div class="row-text">
+                <h5>Sprint 3</h5>
+            </div>
+            <div class="row-icon">
+                <i class="ri-github-fill"></i>
+            </div>
+            <h4>gemaakt door hamza</h4>
+        </div>
+
+</section>
+
+
+<!---contact-->
+<section class="contact" id="contact">
+    <div class="center-text">
+        <h2>Contact <span>Me</span></h2>
+    </div>
+    <div class="content-form">
+        <form action="">
+            <input type="text" placeholder="Your name">
+            <input type="email" placeholder="Email Address"required>
+            <textarea name="" id="" clos="30" rows="10" placeholder="Wirte message here..." required></textarea>
+            <input type="submit" name="" value="Send message" class="send-btn" >
+        </form>
+    </div>
+</section>
+<!-- Js link -->
+<script src="js/script.js"></script>
+
+
+
+</body>
+</html>
+    
